98ca5900736e4310ea71d228cdcb0c8294709afc
Juri <thommenju@gmail.com>
2017-09-18 22:41:22 +0100

    Add primitive types

--- types-operators/app.js
+++ types-operators/app.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+// Primitive Types
+
+// undefined = represents lack of existence (don't set a variable to undefined)
+// null = represents lack of existence (can set variable to this)
+// boolean = true or false
+// number = Floating point number (there's always some decimals). Only one number type in js
+// string = a sequence of characters (both in "" or '')
+// symbol = Used in ES6 --> Read up on this
